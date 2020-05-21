1b5334fc755b911c47aeb6d9b16381ff7ae83d42
Oleg Tsoy <oleg.tcoi.work@gmail.com>
2020-05-21 12:19:32 +0300

    Spring security Auth 5

--- src/main/java/ru/lighthouse/auth/App.java
+++ src/main/java/ru/lighthouse/auth/App.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @SpringBootApplication
 public class App {
@@ -16,14 +19,49 @@ public class App {
     @Value("${service.instance.id}")
     private int instanceId;
 
-    @GetMapping("/instanceid")
+    @GetMapping("/instance-id")
     public Integer getServiceInstanceId() {
         return instanceId;
+    }
+
+    @GetMapping("/local-ip")
+    public Instance getLocalIp() throws UnknownHostException {
+        String localIp = InetAddress.getLocalHost().getHostAddress();
+        return new Instance(localIp, instanceId);
     }
 
     @GetMapping("/testservice")
     public String helloGradle() {
         System.out.println("Hello auth!");
         return "Hello auth!";
+    }
+
+    private class Instance {
+        private String localIp;
+        private int instanceId;
+
+        public Instance() {
+        }
+
+        public Instance(String localIp, int instanceId) {
+            this.localIp = localIp;
+            this.instanceId = instanceId;
+        }
+
+        public String getLocalIp() {
+            return localIp;
+        }
+
+        public void setLocalIp(String localIp) {
+            this.localIp = localIp;
+        }
+
+        public int getInstanceId() {
+            return instanceId;
+        }
+
+        public void setInstanceId(int instanceId) {
+            this.instanceId = instanceId;
+        }
     }
 }
