2ecac21201c5ce03a4759032a65091382a08b6a6
tsoyolv <oleg.tcoi.work@gmail.com>
2021-03-23 21:26:38 +0300

    Added eureka support

--- src/main/java/ru/lighthouse/auth/App.java
+++ src/main/java/ru/lighthouse/auth/App.java
@@ -2,9 +2,11 @@ package ru.lighthouse.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient // for Spring Cloud (delete if used kubernetes)
 @RestController
 @SpringBootApplication
 public class App {
--- src/main/java/ru/lighthouse/auth/integration/IntegrationConfig.java
+++ src/main/java/ru/lighthouse/auth/integration/IntegrationConfig.java
@@ -1,5 +1,6 @@
 package ru.lighthouse.auth.integration;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class IntegrationConfig {
     public static final String ROLE_INTEGRATION = "ROLE_INTEGRATION";
 
+    @LoadBalanced // for Spring Cloud (delete if used kubernetes)
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
